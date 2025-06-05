eb05b085a6bd36d19fcbb59626a6ea02be390251
Dee Mann <93868236+vdeemann@users.noreply.github.com>
2025-06-05 14:25:54 -0700

    Delete guix-shell.sh

--- guix-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Helper script to enter Guix environment
-if command -v guix &> /dev/null; then
-    echo "Entering Guix development environment..."
-    exec guix shell --manifest=manifest.scm
-else
-    echo "Guix not available, using system environment"
-    exec bash
-fi
