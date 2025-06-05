e04f5aac6dec82fa043830e0552d546aaab2a645
Dee Mann <93868236+vdeemann@users.noreply.github.com>
2025-06-05 13:20:02 -0700

    Create manifest.scm

---
+++ manifest.scm
@@ -0,0 +1,17 @@
+;; Guix manifest for Elixir Phoenix development
+(specifications->manifest
+ '("elixir"
+   "erlang"
+   "node"
+   "npm"
+   "postgresql"
+   "git"
+   "curl"
+   "wget"
+   "make"
+   "gcc-toolchain"
+   "pkg-config"
+   "openssl"
+   "zlib"
+   "libffi"
+   "ncurses"))
